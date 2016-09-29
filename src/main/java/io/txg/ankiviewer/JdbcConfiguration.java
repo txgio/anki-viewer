2c999406da57be213be13c7fb91de4a4c4101e74
Tomás Godoi <tsmgodoi@gmail.com>
2016-09-29 09:14:17 -0300

    Fazendo os testes passarem.

---
+++ src/main/java/io/txg/ankiviewer/JdbcConfiguration.java
@@ -0,0 +1,20 @@
+package io.txg.ankiviewer;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+
+@Configuration
+public class JdbcConfiguration {
+
+	@Bean
+	public DataSource dataSource() {
+		SingleConnectionDataSource ds = new SingleConnectionDataSource();
+		ds.setDriverClassName("org.sqlite.JDBC");
+		ds.setUrl("jdbc:sqlite:./data/collection.anki2");
+		return ds;
+	}
+	
+}
--- src/main/java/io/txg/ankiviewer/TagRepositorySqliteJdbcImpl.java
+++ src/main/java/io/txg/ankiviewer/TagRepositorySqliteJdbcImpl.java
@@ -1,15 +1,28 @@
 package io.txg.ankiviewer;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JsonParser;
+import org.springframework.boot.json.JsonParserFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class TagRepositorySqliteJdbcImpl implements TagRepository {
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	private JsonParser jsonParser = JsonParserFactory.getJsonParser();
+
 	@Override
 	public List<Tag> findAll() {
-		return null;
+		return jdbcTemplate
+				.queryForObject("SELECT tags FROM col WHERE id = 1",
+						(rs, rowNum) -> jsonParser.parseMap(rs.getString("tags")).keySet())
+				.stream().map((nomeTag) -> new Tag(nomeTag)).collect(Collectors.toList());
 	}
 
 }
