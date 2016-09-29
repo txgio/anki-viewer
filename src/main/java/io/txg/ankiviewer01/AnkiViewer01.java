2a2ea5a15821b843b12039a3a908ad9c8a84af60
Tomás Godoi <tsmgodoi@gmail.com>
2016-09-29 08:04:44 -0300

    Versão inicial do sistema.

---
+++ src/main/java/io/txg/ankiviewer01/AnkiViewer01.java
@@ -0,0 +1,13 @@
+package io.txg.ankiviewer01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class AnkiViewer01 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnkiViewer01.class, args);
+	}
+
+}
